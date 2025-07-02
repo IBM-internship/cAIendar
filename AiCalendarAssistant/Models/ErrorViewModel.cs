6af18aa79f86dbeadfb264f2c5daa9732c8d7258
projectraya <rminkova007@gmail.com>
2025-07-02 17:59:25 +0300

    Initial commit

---
+++ AiCalendarAssistant/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AiCalendarAssistant.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
