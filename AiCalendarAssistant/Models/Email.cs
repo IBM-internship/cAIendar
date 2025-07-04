ef7e45318d6abc729200171aa02f1fb206a5c213
Misho12345 <mihailgeorgievgeorgiev1000@gmail.com>
2025-07-04 16:41:08 +0300

    Add Emails feature with Gmail integration and update layout

---
+++ AiCalendarAssistant/Models/Email.cs
@@ -0,0 +1,9 @@
+﻿namespace AiCalendarAssistant.Models;
+
+public class Email
+{
+    public DateTime Date { get; set; }
+    public string From { get; set; } = string.Empty;
+    public string Subject { get; set; } = string.Empty;
+    public string Snippet { get; set; } = string.Empty;
+}
