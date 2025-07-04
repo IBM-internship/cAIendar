2f916970ef57f6a4219ca2f9b3cee5c803f938b8
projectraya <rminkova007@gmail.com>
2025-07-04 13:25:44 +0300

    Default main - everything working

--- AiCalendarAssistant/Program.cs
+++ AiCalendarAssistant/Program.cs
@@ -29,7 +29,7 @@ namespace AiCalendarAssistant
 
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
             
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => 
+            builder.Services.AddDefaultIdentity<ApplicationUser>(options => 
                 {
                     options.SignIn.RequireConfirmedAccount = false;
                     options.Password.RequireDigit = false;
