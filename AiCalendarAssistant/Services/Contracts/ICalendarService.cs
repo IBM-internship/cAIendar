6159f2307eab5f21f155a849266dcad38a6603e1
projectraya <rminkova007@gmail.com>
2025-07-08 19:21:56 +0300

    Landing page upgrade

--- AiCalendarAssistant/Services/Contracts/ICalendarService.cs
+++ AiCalendarAssistant/Services/Contracts/ICalendarService.cs
@@ -1,15 +1,17 @@
 using AiCalendarAssistant.Data.Models;
+using System.Linq.Expressions;
 
 namespace AiCalendarAssistant.Services.Contracts
 {
-    public interface ICalendarService
-    {
-        Task AddEventAsync(Event calendarEvent);
-        Task<bool> DeleteEventAsync(int eventId);
-        Task<Event?> GetEventByIdAsync(int eventId);
-        Task<bool> ReplaceEventAsync(Event updatedEvent);
-        Task<List<Event>> GetAllEventsAsync();
-        Task<List<Event>> GetEventsAsync(Func<Event, bool> predicate);
-        Task<List<Event>> GetEventsInTimeRangeAsync(DateTime start, DateTime end);
-    }
+	public interface ICalendarService
+	{
+		Task AddEventAsync(Event calendarEvent);
+		Task<bool> DeleteEventAsync(int eventId);
+		Task<Event?> GetEventByIdAsync(int eventId);
+		Task<bool> ReplaceEventAsync(Event updatedEvent);
+		Task<List<Event>> GetAllEventsAsync();
+		Task<List<Event>> GetEventsAsync(Func<Event, bool> predicate);
+		Task<List<Event>> GetEventsInTimeRangeAsync(DateTime start, DateTime end);
+	}
+
 }
