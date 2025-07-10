771a40d38085ed209d791e62c3d5a3d40cd70fb2
DebelToni <anton.ivanov.hristov@gmail.com>
2025-07-10 15:40:00 +0300

    chat needs title

--- AiCalendarAssistant.Data/Models/Chat.cs
+++ AiCalendarAssistant.Data/Models/Chat.cs
@@ -4,6 +4,7 @@ namespace AiCalendarAssistant.Data.Models
     {
         public int Id { get; set; }
         public IEnumerable<Message>? Messages { get; set; }
+		public string Title { get; set; } = "New Chat";
 
         public string? UserId { get; set; }
         public ApplicationUser? User { get; set; }
