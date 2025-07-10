47a789cdea654e161b9ad0922b918b5d99514589
Misho12345 <mihailgeorgievgeorgiev1000@gmail.com>
2025-07-10 23:36:03 +0300

    removed } so no error

--- AiCalendarAssistant.Data/Configuration/EventConfiguration.cs
+++ AiCalendarAssistant.Data/Configuration/EventConfiguration.cs
@@ -2,23 +2,22 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace AiCalendarAssistant.Data.Configuration
-{
-    public class EventConfiguration : IEntityTypeConfiguration<Event>
-    {
-        public void Configure(EntityTypeBuilder<Event> builder)
-        {
-            builder
-                .HasOne(e => e.User)
-                .WithMany(u => u.Events)
-                .HasForeignKey(e => e.UserId)
-                .OnDelete(DeleteBehavior.Cascade);
+namespace AiCalendarAssistant.Data.Configuration;
 
-            builder
-                .HasOne(e => e.EventCreatedFromEmail)
-                .WithOne(e => e.EmailCreatedEvent)
-                .HasForeignKey<Event>(e => e.EventCreatedFromEmailId) // FK is in Event
-                .OnDelete(DeleteBehavior.Restrict);
-        }
+public class EventConfiguration : IEntityTypeConfiguration<Event>
+{
+    public void Configure(EntityTypeBuilder<Event> builder)
+    {
+        builder
+            .HasOne(e => e.User)
+            .WithMany(u => u.Events)
+            .HasForeignKey(e => e.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder
+            .HasOne(e => e.EventCreatedFromEmail)
+            .WithOne(e => e.EmailCreatedEvent)
+            .HasForeignKey<Event>(e => e.EventCreatedFromEmailId) // FK is in Event
+            .OnDelete(DeleteBehavior.Restrict);
     }
 }
--- AiCalendarAssistant.Data/Models/Chat.cs
+++ AiCalendarAssistant.Data/Models/Chat.cs
@@ -6,8 +6,7 @@ public class Chat
     public IEnumerable<Message>? Messages { get; set; }
     public string Title { get; set; } = "New Chat";
 
-        public string? UserId { get; set; }
-        public ApplicationUser? User { get; set; }
-        public bool IsDeleted { get; set; } = false; // Flag to mark user as deleted without removing from database
-    }
+    public string? UserId { get; set; }
+    public ApplicationUser? User { get; set; }
+    public bool IsDeleted { get; set; } = false; // Flag to mark user as deleted without removing from database
 }
