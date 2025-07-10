9a1e5fbbc18985b1479cf10c20e2229c7895277b
Tony555x <antonkremov2007@gmail.com>
2025-07-10 19:09:16 +0300

    task migration 2

--- AiCalendarAssistant.Data/Models/Task.cs
+++ AiCalendarAssistant.Data/Models/Task.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace AiCalendarAssistant.Data.Models
 {
     public class UserTask
     {
+        [Key]
         public int Id { get; set; } 
 
         public string Title { get; set; } = null!; 
@@ -9,7 +13,8 @@ namespace AiCalendarAssistant.Data.Models
         public string? Description { get; set; } 
 
         public string? Color { get; set; } 
-
+            
+        [ForeignKey(nameof(User))]
         public string? UserId { get; set; } 
         public ApplicationUser? User { get; set; } 
 		public Importance Importance { get; set; } = Importance.Medium; 
