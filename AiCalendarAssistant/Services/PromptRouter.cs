03af3bb45e49dfb42f843dd3d3c1e281facbcf41
DebelToni <anton.ivanov.hristov@gmail.com>
2025-07-11 11:40:40 +0300

    no ioption

--- AiCalendarAssistant/Services/PromptRouter.cs
+++ AiCalendarAssistant/Services/PromptRouter.cs
@@ -5,11 +5,11 @@ using Microsoft.Extensions.Options;
 
 namespace AiCalendarAssistant.Services;
 
-public sealed class PromptRouter(WatsonxClient w, OllamaClient o, IOptions<LlmSettings> cfg)
+public sealed class PromptRouter(WatsonxClient w, OllamaClient o, LlmSettings cfg)
 {
     private readonly ILlmClient  _watsonx = w;
     private readonly ILlmClient  _ollama = o;
-    private readonly LlmSettings _cfg = cfg.Value;
+    private readonly LlmSettings _cfg = cfg;
 
     public Task<PromptResponse> SendAsync(PromptRequest req, CancellationToken ct = default)
         => (_cfg.UseOllama ? _ollama : _watsonx).SendAsync(req, ct);
