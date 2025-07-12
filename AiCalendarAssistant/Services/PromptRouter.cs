48ae6281a2ebd201aa9c0c7fe8b7b7d4a4d1ddc2
DebelToni <anton.ivanov.hristov@gmail.com>
2025-07-12 23:43:12 +0300

    added tool support for watsonx + prompt router needs finishing

--- AiCalendarAssistant/Services/PromptRouter.cs
+++ AiCalendarAssistant/Services/PromptRouter.cs
@@ -11,15 +11,28 @@ public sealed class PromptRouter(WatsonxClient w, OllamaClient o, LlmSettings cf
     private readonly ILlmClient  _watsonx = w;
     private readonly ILlmClient  _ollama = o;
     private readonly LlmSettings _cfg = cfg;
-	private readonly string? additionalInstructions = "\nExtra info:\n- Today is " 
+	private string? additionalInstructions = "\nExtra info:\n- Today is " 
         + DateTime.Now.ToString("yyyy-MM-dd") 
+		+ " at " + DateTime.Now.AddHours(3).ToString("HH:mm:ss")
         + " (" + DateTime.Now.DayOfWeek + ")";
+	// ad duser data here
+
+	private void UpdateTime(){
+		additionalInstructions = "\nExtra info:\n- Today is " 
+			+ DateTime.Now.ToString("yyyy-MM-dd")
+			+ " at " + DateTime.Now.AddHours(3).ToString("HH:mm:ss")
+			+ " (" + DateTime.Now.DayOfWeek + ")"
+			+ ApplicationDbContext.GetUserD
+		// add user data here
+	}
     public Task<PromptResponse> SendAsync(PromptRequest req, CancellationToken ct = default)
     {
+		UpdateTime();
         for (var i = 0; i < 3; i++)
         {
             try
             {
+				Console.WriteLine($"Additional Instructions: {additionalInstructions}");
                 var o = (_cfg.UseOllama ? _ollama : _watsonx).SendAsync(req, ct, additionalInstructions);
                 return o;
             }
