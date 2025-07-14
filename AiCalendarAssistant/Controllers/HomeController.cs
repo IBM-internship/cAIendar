c683a9ad9bb6671983a5422327c68c14c4817fb8
ViktoriaBPetrova <vickiki036@gmail.com>
2025-07-14 07:59:22 +0300

    Changed the UI

--- AiCalendarAssistant/Controllers/HomeController.cs
+++ AiCalendarAssistant/Controllers/HomeController.cs
@@ -1,10 +1,13 @@
-using System.Diagnostics;
+using AiCalendarAssistant.Data;
 using AiCalendarAssistant.Models;
 using AiCalendarAssistant.Services;
 using AiCalendarAssistant.Services.Contracts;
 using Google.Apis.Gmail.v1;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using System.Security.Claims;
 
 namespace AiCalendarAssistant.Controllers
 {
@@ -12,16 +15,35 @@ namespace AiCalendarAssistant.Controllers
 	public class HomeController : BaseController
 	{
 		private readonly IGmailEmailService _gmail;
+		private readonly ApplicationDbContext db;
 
-		public HomeController(IGmailEmailService gmail)
+		public HomeController(IGmailEmailService gmail, ApplicationDbContext _db)
 		{
 			_gmail = gmail;
+			db = _db;
 		}
 
-		public IActionResult Index()
+		public async Task<IActionResult> Index(int? selectedChatId = null)
 		{
-			return View();
-		}
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var chats = await db.Chats
+                .Include(c => c.Messages.OrderBy(m => m.Pos))
+                .Where(c => c.UserId == userId)
+                .ToListAsync();
+
+            var selectedChat = selectedChatId.HasValue
+                ? chats.FirstOrDefault(c => c.Id == selectedChatId)
+                : chats.FirstOrDefault();
+
+            ViewData["SelectedChatId"] = selectedChat?.Id;
+
+            return View(new ChatViewModel
+            {
+                Chats = chats,
+                SelectedChat = selectedChat
+            });
+        }
 
 		public IActionResult Calendar()
 		{
